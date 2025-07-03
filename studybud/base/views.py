0e91f27a3a9a2c500abd82ec591d286242ba17d9
Ceejay-777 <covenantcrackslord01@gmail.com>
2025-07-03 21:30:26 +0100

    templating

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -8,6 +8,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from .forms import UserForm
 
 def home(request):
     query = request.GET.get('query') if request.GET.get('query') != None else ''
@@ -80,6 +81,11 @@ def updateRoom(request, pk):
     if request.method == 'POST':
         topic_name = request.POST.get('topic')
         topic, created = Topic.objects.get_or_create(name=topic_name)
+        
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
         
         return redirect('home')
     
@@ -161,3 +167,17 @@ def deleteMessage(request, pk):
         return redirect(f'room', pk=message.room.id)
     
     return render(request, 'base/delete.html', {'obj': message})
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+    
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+        
+    context  = {'form': form}
+    return render(request, 'base/update-user.html', context)
