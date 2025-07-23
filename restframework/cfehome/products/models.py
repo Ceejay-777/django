6a5995af6ade4d5d88b0bdcbf74da14a1b979a02
Ceejay-777 <covenantcrackslord01@gmail.com>
2025-07-23 08:18:20 +0100

    pagination

--- restframework/cfehome/products/models.py
+++ restframework/cfehome/products/models.py
@@ -1,15 +1,26 @@
 from django.db import models
 from django.conf import settings
+from django.db.models import Q
 
 User = settings.AUTH_USER_MODEL
 
 class ProductQuerySet(models.QuerySet):
     def is_public(self):
         return self.filter(public=True)
+    
+    def search(self, query, user=None):
+        lookup = Q(title__icontains=query) | Q(content__icontains=query)
+        qs = self.filter(lookup)
+        if user is not None:
+            qs = qs.filter(user=user)
+        return qs
 
 class ProductManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(self.model, using=self._db)
+    
     def search(self, query, user=None):
-        return Product.objects.filter(public=True, title__icontains=query)
+        return self.get_queryset().is_public().search(query, user=user)
 
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, default=1)
@@ -27,3 +38,17 @@ class Product(models.Model):
     
     def get_discount(self):
         return '122'
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
